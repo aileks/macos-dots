5848367d1986cee61f3196e02842709e4d0f1e9e
aileks <88669313+aileks@users.noreply.github.com>
2024-01-29 19:36:06 -0500

    added some more plugisn

---
+++ .config/nvim/lua/plugins/floaterm.lua
@@ -0,0 +1,12 @@
+return {
+  "voldikss/vim-floaterm",
+  keys = {
+    { "7", ":FloatermToggle<CR>" },
+    { "7", "<C-\\><C-n>:FloatermToggle<CR>", mode = "t" },
+  },
+  cmd = { "FloatermToggle" },
+  init = function()
+    vim.g.floaterm_width = 0.8
+    vim.g.floaterm_height = 0.8
+  end,
+}
