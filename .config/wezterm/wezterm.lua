386539023e7ece507e6864b0c763d6069338d850
aileks <88669313+aileks@users.noreply.github.com>
2024-02-15 18:52:46 -0500

    changed colorschemes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,14 +1,11 @@
 -- bootstrapping
 local wezterm = require 'wezterm'
-local colors = require('lua/rose-pine').colors()
-local window_frame = require('lua/rose-pine').window_frame()
 local config = {}
 
 -- actual config
 config.font = wezterm.font("RecMonoLinear Nerd Font")
 config.font_size = 16.0
-config.colors = colors
-config.window_frame = window_frame
+config.color_scheme = "Monokai Soda (Gogh)"
 config.window_background_opacity = 0.90
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "NeverPrompt"
