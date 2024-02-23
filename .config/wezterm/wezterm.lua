975cd868becd9f5fa5bf82afc08c07fc66845795
aileks <88669313+aileks@users.noreply.github.com>
2024-02-23 10:15:01 -0500

    wezterm theme and font change

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,12 +1,17 @@
 -- bootstrapping
 local wezterm = require 'wezterm'
+local colors = require('lua/monokai-pro-octagon').colors()
+local window_frame = require('lua/monokai-pro-octagon').window_frame()
 local config = {}
 
 -- actual config
-config.font = wezterm.font("RecMonoLinear Nerd Font")
-config.font_size = 16.0
-config.color_scheme = "Monokai Soda (Gogh)"
-config.window_background_opacity = 0.90
+config.font = wezterm.font("Rec Mono Duotone")
+config.font_size = 18.0
+config.colors = colors
+config.colors.selection_fg = '#b2b9bd'
+config.colors.selection_bg = '#33374b'
+config.window_frame = window_frame
+config.window_background_opacity = 0.95
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "NeverPrompt"
 
