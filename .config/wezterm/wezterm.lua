881a112d855a9aa51c1b09171c28c34fc34822d7
Aaliyah Harper <aeverly14@pm.me>
2024-03-18 10:56:56 -0400

    uwu

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local config = {}
 -- actual config
 config.font = wezterm.font('Rec Mono Linear')
 config.font_size = 12.0
-config.color_scheme = 'Oxocarbon Dark (Gogh)'
+config.color_scheme = 'Catppuccin Mocha'
 config.enable_tab_bar = false
 config.window_background_opacity = 0.95
 config.window_decorations = 'RESIZE'
