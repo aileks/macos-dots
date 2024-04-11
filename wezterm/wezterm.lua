b799ea7cd0fc6715a92ff16c9a81b6cd908e668e
aileks <aeverly14@pm.me>
2024-04-11 07:13:05 -0400

    wezterm font size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local config = {}
 
 -- actual config
 config.font = wezterm.font('RecMonoLinear Nerd Font')
-config.font_size = 12.0
+config.font_size = 12.5
 config.color_scheme = 'Catppuccin Mocha'
 config.enable_tab_bar = false
 config.window_background_opacity = 0.95
