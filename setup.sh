1bbc7d0f299c1735d154c3fe3bd18e0f9e936b6c
aileks <aeverly14@pm.me>
2024-04-13 18:11:59 -0400

    no more sway...also back on arch

--- setup.sh
+++ setup.sh
@@ -9,8 +9,6 @@ rm -rf $HOME/.config/nvim
 ln -s $DOTDIR/nvim $HOME/.config/nvim
 rm -rf $HOME/.config/i3
 ln -s $DOTDIR/i3 $HOME/.config/i3
-rm -rf $HOME/.config/sway
-ln -s $DOTDIR/sway $HOME/.config/sway
 rm -rf $HOME/.config/i3blocks
 ln -s $DOTDIR/i3blocks $HOME/.config/i3blocks
 rm -rf $HOME/.config/picom
@@ -19,6 +17,10 @@ rm -rf $HOME/.config/wezterm
 ln -s $DOTDIR/wezterm $HOME/.config/wezterm
 rm -rf $HOME/.config/dunst
 ln -s $DOTDIR/dunst $HOME/.config/dunst
+rm -rf $HOME/.gitconfig
+ln -s $DOTDIR/git/gitconfig $HOME/.gitconfig
+rm -rf $HOME/.gitignore_global
+ln -s $DOTDIR/git/gitignore_global $HOME/.gitignore_global
 
 # theme stuff
 cp -r $DOTDIR/.local $HOME
