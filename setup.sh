f961cc01a9c686dcc7314be2ccd11d11efd1634f
aileks <aeverly14@pm.me>
2024-04-19 07:45:57 -0400

    added ghostty config

--- setup.sh
+++ setup.sh
@@ -15,6 +15,8 @@ rm -rf $HOME/.config/picom
 ln -s $DOTDIR/picom $HOME/.config/picom
 rm -rf $HOME/.config/wezterm
 ln -s $DOTDIR/wezterm $HOME/.config/wezterm
+rm -rf $HOME/.config/ghostty
+ln -s $DOTDIR/ghostty $HOME/.config/ghostty
 rm -rf $HOME/.config/dunst
 ln -s $DOTDIR/dunst $HOME/.config/dunst
 rm -rf $HOME/.gitconfig
