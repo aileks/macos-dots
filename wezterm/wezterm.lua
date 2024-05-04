1f8da215be231bf2f4a3b764302e00835ad94dd3
aileks <aeverly14@pm.me>
2024-05-04 10:36:06 -0400

    gentoo changes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,11 +3,11 @@ local wezterm = require 'wezterm'
 local config = {}
 
 -- actual config
-config.font = wezterm.font('RecMonoLinear Nerd Font')
-config.font_size = 12.5
+config.font = wezterm.font('MonoLisa')
+config.font_size = 12
 config.color_scheme = 'Catppuccin Mocha'
 config.enable_tab_bar = false
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.90
 config.window_decorations = 'RESIZE'
 config.window_close_confirmation = 'NeverPrompt'
 
