696c7e33bcfe362ac66d6cc27d1892d96348cb28
aileks <aeverly14@pm.me>
2024-05-14 12:08:35 -0400

    nvim cleanup

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -45,68 +45,6 @@ return {
       },
     })
 
-    -- PHP
-    require('lspconfig').intelephense.setup({
-      commands = {
-        IntelephenseIndex = {
-          function()
-            vim.lsp.buf.execute_command({ command = 'intelephense.index.workspace' })
-          end,
-        },
-      },
-      on_attach = function(client, bufnr)
-        -- client.server_capabilities.documentFormattingProvider = false
-        -- client.server_capabilities.documentRangeFormattingProvider = false
-        -- if client.server_capabilities.inlayHintProvider then
-        --   vim.lsp.buf.inlay_hint(bufnr, true)
-        -- end
-      end,
-      capabilities = capabilities
-    })
-
-    require('lspconfig').phpactor.setup({
-      capabilities = capabilities,
-      on_attach = function(client, bufnr)
-        client.server_capabilities.completionProvider = false
-        client.server_capabilities.hoverProvider = false
-        client.server_capabilities.implementationProvider = false
-        client.server_capabilities.referencesProvider = false
-        client.server_capabilities.renameProvider = false
-        client.server_capabilities.selectionRangeProvider = false
-        client.server_capabilities.signatureHelpProvider = false
-        client.server_capabilities.typeDefinitionProvider = false
-        client.server_capabilities.workspaceSymbolProvider = false
-        client.server_capabilities.definitionProvider = false
-        client.server_capabilities.documentHighlightProvider = false
-        client.server_capabilities.documentSymbolProvider = false
-        client.server_capabilities.documentFormattingProvider = false
-        client.server_capabilities.documentRangeFormattingProvider = false
-      end,
-      init_options = {
-        ["language_server_phpstan.enabled"] = false,
-        ["language_server_psalm.enabled"] = false,
-      },
-      handlers = {
-        ['textDocument/publishDiagnostics'] = function() end
-      }
-    })
-
-    -- TypeScript
-    require('lspconfig').tsserver.setup({
-      on_attach = function(client, bufnr)
-        -- client.server_capabilities.documentFormattingProvider = false
-        -- client.server_capabilities.documentRangeFormattingProvider = false
-        -- if client.server_capabilities.inlayHintProvider then
-        --   vim.lsp.buf.inlay_hint(bufnr, true)
-        -- end
-      end,
-      capabilities = capabilities,
-      filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
-    })
-
-    -- Tailwind CSS
-    require('lspconfig').tailwindcss.setup({ capabilities = capabilities })
-
     -- JSON
     require('lspconfig').jsonls.setup({
       capabilities = capabilities,
@@ -152,24 +90,6 @@ return {
           })
         end
       end,
-    })
-
-    -- Prettier
-    require('prettier').setup({
-      bin = 'prettierd',
-      filetypes = {
-        'javascript',
-        'javascriptreact',
-        'typescript',
-        'typescriptreact',
-        'vue',
-        'html',
-        'css',
-        'scss',
-        'json',
-        'yaml',
-        'markdown',
-      }
     })
 
     require('mason-null-ls').setup({ automatic_installation = true })
