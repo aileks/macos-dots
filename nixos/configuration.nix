883c6bf708e6b2ed65ef143ef74b3b1f7c1cfc44
aileks <aeverly14@pm.me>
2024-05-14 13:31:46 -0400

    nix-ld configuration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -225,6 +225,16 @@
     };
     nix-ld = {
       enable = true;
+      libraries = with pkgs; [
+        stdenv.cc.cc
+        zlib
+        fuse3
+        icu
+        nss
+        openssl
+        curl
+        expat
+      ];
     };
   };
 
