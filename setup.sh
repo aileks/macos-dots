0ad5e639e5fd05869fb7908e0221c93375aef806
aileks <aeverly14@pm.me>
2024-05-16 12:57:44 -0400

    macos branch

--- setup.sh
+++ setup.sh
@@ -1,35 +1,25 @@
 #!/usr/bin/env bash
+# !!! THIS IS THE macOS VERSION !!!
+
 # get dotdir (this directory)
 DOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# theme stuff
-cp -r $DOTDIR/.local $HOME
-cp -r $DOTDIR/themes/* $HOME/.config
-cp $DOTDIR/.Xresources $HOME
 
 # symlinks
 ln -sf $DOTDIR/tmux/tmux.conf $HOME/.tmux.conf
 ln -sf $DOTDIR/zsh/zshrc $HOME/.zshrc
 rm -rf $HOME/.config/nvim
 ln -s $DOTDIR/nvim $HOME/.config/nvim
-rm -rf $HOME/.config/i3
-ln -s $DOTDIR/i3 $HOME/.config/i3
-rm -rf $HOME/.config/i3blocks
-ln -s $DOTDIR/i3blocks $HOME/.config/i3blocks
-rm -rf $HOME/.config/picom
-ln -s $DOTDIR/picom $HOME/.config/picom
 rm -rf $HOME/.config/wezterm
 ln -s $DOTDIR/wezterm $HOME/.config/wezterm
 rm -rf $HOME/.config/doom
 ln -s $DOTDIR/doom $HOME/.config/doom
-rm -rf $HOME/.config/dunst
-ln -s $DOTDIR/dunst $HOME/.config/dunst
-rm -rf $HOME/.config/doom
-cp -r $DOTDIR/doom $HOME/.config/doom
+rm -rf $HOME/.config/skhd
+ln -s $DOTDIR/skhd $HOME/.config/skhd
+rm -rf $HOME/.config/yabai
+ln -s $DOTDIR/yabai $HOME/.config/yabai
+rm -rf $HOME/.config/sketchybar/
+ln -s $DOTDIR/sketchybar $HOME/.config/sketchybar
 
 echo ""
-echo "ALL DONE!"
-echo "Now rebuild!"
-echo ""
-echo "Remember to copy git config files...you know who you are..."
+echo "All done!"
 echo ""
