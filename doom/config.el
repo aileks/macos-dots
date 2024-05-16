3d49390f5d68364813a3d9c1e9406794a3645a6c
aileks <aeverly14@pm.me>
2024-05-16 18:23:46 -0400

    switched to amethyst because yabai is broken

--- doom/config.el
+++ doom/config.el
@@ -21,8 +21,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "MonoLisa" :size 18 :weight 'medium)
-      doom-variable-pitch-font (font-spec :family "Ubuntu" :size 18))
+(setq doom-font (font-spec :family "MartianMono Nerd Font" :size 16)
+      doom-variable-pitch-font (font-spec :family "Ubuntu" :size 16))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
