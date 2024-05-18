8e9c93717e321f5aac29e4542ba444ce6c707467
aileks <aeverly14@pm.me>
2024-05-18 18:22:04 -0400

    fixed yabairc error

--- setup.sh
+++ setup.sh
@@ -9,16 +9,16 @@ ln -sf $DOTDIR/tmux/tmux.conf $HOME/.tmux.conf
 ln -sf $DOTDIR/zsh/zshrc $HOME/.zshrc
 rm -rf $HOME/.config/nvim
 ln -s $DOTDIR/nvim $HOME/.config/nvim
-rm -rf $HOME/.config/yabai
-cp -r $DOTDIR/yabai $HOME/.config/yabai
-rm -rf $HOME/.config/wezterm
-ln -s $DOTDIR/wezterm $HOME/.config/wezterm
 rm -rf $HOME/.config/doom
 ln -s $DOTDIR/doom $HOME/.config/doom
 rm -rf $HOME/.config/skhd
 ln -s $DOTDIR/skhd $HOME/.config/skhd
 rm -rf $HOME/.config/sketchybar
-ln -sf $DOTDIR/sketchybar $HOME/.config/sketchybar
+ln -s $DOTDIR/sketchybar $HOME/.config/sketchybar
+rm -rf $HOME/.config/yabai
+ln -s $DOTDIR/yabai $HOME/.config/yabai
+rm -rf $HOME/.config/wezterm
+ln -s $DOTDIR/wezterm $HOME/.config/wezterm
 
 echo ""
 echo "All done!"
