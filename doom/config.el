affcdcc988df23352f297057bc3cc47d916d1b23
aileks <aeverly14@pm.me>
2024-05-22 11:16:54 -0400

    opacity is broken, will fix later

--- doom/config.el
+++ doom/config.el
@@ -1,3 +1,7 @@
+(set-frame-parameter (selected-frame) 'alpha '(95 . 95))
+(add-to-list 'default-frame-alist '(alpha . (95 . 95)))
+(add-to-list 'default-frame-alist '(undecorated-round . t))
+
 (setq doom-font (font-spec :family "MartianMono Nerd Font" :size 15)
       doom-variable-pitch-font (font-spec :family "SF Pro" :size 16))
 
@@ -19,6 +23,3 @@
 (setq lsp-solargraph-references t)
 (setq lsp-solargraph-rename t)
 (setq lsp-solargraph-symbols t)
-
-(set-frame-parameter (selected-frame) 'alpha '(95 . 95))
-(add-to-list 'default-frame-alist '(alpha . (95 . 95)))
