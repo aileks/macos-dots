0591c217530bd891e3e989df79775e59185abc33
aileks <aeverly14@pm.me>
2024-06-11 11:32:04 -0400

    wezterm font change

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,8 +3,8 @@ local wezterm = require 'wezterm'
 local config = {}
 
 -- actual config
-config.font = wezterm.font('MartianMono Nerd Font')
-config.font_size = 14
+config.font = wezterm.font('MesloLGSDZ Nerd Font Mono')
+config.font_size = 16
 config.color_scheme = 'Catppuccin Mocha'
 config.enable_tab_bar = false
 config.window_background_opacity = 0.90
