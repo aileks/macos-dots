1a3d5824f3de28573ab3493ed21e95109f947d0d
aileks <aeverly14@pm.me>
2024-06-13 16:26:43 -0400

    wezterm theme change

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local config = {}
 -- actual config
 config.font = wezterm.font('MesloLGS Nerd Font Mono', { weight = "Regular" })
 config.font_size = 16
-config.color_scheme = 'Geohot (Gogh)'
+config.color_scheme = 'Catppuccin Mocha'
 config.enable_tab_bar = false
 config.window_background_opacity = 0.90
 config.window_decorations = 'RESIZE'
