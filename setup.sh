fc380a64be906240fee32f8961c72e9304699a6e
aileks <aeverly14@pm.me>
2024-06-15 13:04:09 -0400

    commented out sketchybar and yabai in setup.sh as I don't use them anymore

--- setup.sh
+++ setup.sh
@@ -10,12 +10,13 @@ rm -rf $HOME/.config/nvim
 ln -s $DOTDIR/nvim $HOME/.config/nvim
 rm -rf $HOME/.config/skhd
 ln -s $DOTDIR/skhd $HOME/.config/skhd
-rm -rf $HOME/.config/sketchybar
-ln -s $DOTDIR/sketchybar $HOME/.config/sketchybar
-rm -rf $HOME/.config/yabai
-ln -s $DOTDIR/yabai $HOME/.config/yabai
+# rm -rf $HOME/.config/sketchybar
+# ln -s $DOTDIR/sketchybar $HOME/.config/sketchybar
+# rm -rf $HOME/.config/yabai
+# ln -s $DOTDIR/yabai $HOME/.config/yabai
 rm -rf $HOME/.config/wezterm
 ln -s $DOTDIR/wezterm $HOME/.config/wezterm
+
 # zed is a special case
 rm -rf $HOME/.config/zed/keymap.json
 rm -rf $HOME/.config/zed/settings.json
