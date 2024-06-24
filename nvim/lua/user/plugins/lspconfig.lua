9c72fc159b9ae7588171a493ff86642767390c27
aileks <aeverly14@pm.me>
2024-06-24 15:57:16 -0400

    emmet 2

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -117,7 +117,7 @@ return {
 
     -- Emmet
     require('lspconfig').emmet_ls.setup({
-      on_attach = on_attach,
+      -- on_attach = on_attach,
       capabilities = capabilities,
       filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" },
       init_options = {
