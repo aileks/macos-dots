8c581c0b7a8baf9d81155639e4fbc59b6ca9a0bd
aileks <aeverly14@pm.me>
2024-06-24 18:02:29 -0400

    some mason stuff

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -15,9 +15,11 @@ return {
         height = 0.8,
       },
     })
-    require('mason-lspconfig').setup({ automatic_installation = true })
 
-    local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+    require('mason-lspconfig').setup({ automatic_installation = true })
+    require('mason-null-ls').setup({ automatic_installation = true })
+
+    local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
     capabilities.textDocument.completion.completionItem.snippetSupport = true
 
     -- Lua
