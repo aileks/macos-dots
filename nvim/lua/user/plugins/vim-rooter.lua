fbe4664e6e0d91a123b2540fcb29b65ae0abf446
aileks <aeverly14@pm.me>
2024-06-30 16:59:35 -0400

    rooter

--- nvim/lua/user/plugins/vim-rooter.lua
+++ nvim/lua/user/plugins/vim-rooter.lua
@@ -1,3 +1,10 @@
 return {
-  'airblade/vim-rooter'
+  'airblade/vim-rooter',
+  init = function()
+    vim.g.rooter_manual_only = 1
+    vim.g.rooter_patterns = { '.git', 'composer.json' }
+  end,
+  config = function()
+    vim.cmd('Rooter')
+  end,
 }
