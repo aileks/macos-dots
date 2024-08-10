d942c91c140f27496a54a23dd9ae532a2864e248
Aaliyah Harper <aeverly14@pm.me>
2024-08-10 12:56:39 -0400

    zed settings for vim mode

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -138,6 +138,9 @@ return {
       end,
     })
 
+    -- Tailwind
+    lspconfig.tailwindcss.setup({ capabilities = capabilities })
+
     -- none-ls
     local null_ls = require('null-ls')
     local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
