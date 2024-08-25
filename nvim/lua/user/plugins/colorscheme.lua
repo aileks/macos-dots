5d703a79a0ace4ee8ecb0680aa26ab5897feca9d
aileks <aeverly14@pm.me>
2024-08-25 14:09:56 -0400

    changed wezterm and neovim themes

--- nvim/lua/user/plugins/colorscheme.lua
+++ nvim/lua/user/plugins/colorscheme.lua
@@ -1,9 +1,21 @@
 return {
-  'rebelot/kanagawa.nvim',
-  config = function()
-    require('kanagawa').setup({
-      transparent = true,
+  'Shatur/neovim-ayu',
+  config = function ()
+    require('ayu').setup({
+      overrides = {
+        -- Transparency options
+        Normal = { bg = "None" },
+        ColorColumn = { bg = "None" },
+        SignColumn = { bg = "None" },
+        Folded = { bg = "None" },
+        FoldColumn = { bg = "None" },
+        CursorLine = { bg = "None" },
+        CursorColumn = { bg = "None" },
+        WhichKeyFloat = { bg = "None" },
+        VertSplit = { bg = "None" },
+      }
     })
-    vim.cmd.colorscheme('kanagawa-wave')
+
+    vim.cmd.colorscheme('ayu-dark')
   end
 }
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,10 +5,9 @@ local config = {}
 -- actual config
 config.font = wezterm.font('BerkeleyMono Nerd Font')
 config.font_size = 16
-config.color_scheme = 'Kanagawa (Gogh)'
+config.color_scheme = 'Ayu Dark (Gogh)'
 config.enable_tab_bar = false
-config.window_background_opacity = 0.97
-config.window_decorations = 'RESIZE'
+config.window_background_opacity = 0.95
 config.window_close_confirmation = 'NeverPrompt'
 
 return config
