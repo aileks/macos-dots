6c0c78f7c978ad3e0e0c93a41caf3805fdb19ff4
aileks <aeverly14@pm.me>
2024-10-12 17:35:15 -0400

    ideavimrc added to setup script

--- setup.sh
+++ setup.sh
@@ -5,36 +5,14 @@ set -e
 # get dotdir (this directory)
 DOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-check_error() {
-    if [ $? -ne 0 ]; then
-        echo "Error occurred in script. Exiting."
-        exit 1
-    fi
-}
-
 # symlinks
 ln -sf $DOTDIR/tmux/tmux.conf $HOME/.tmux.conf
-check_error
 ln -sf $DOTDIR/zsh/zshrc $HOME/.zshrc
-check_error
+ln -sf $DOTDIR/ideavim/ideavimrc $HOME/.ideavimrc
 rm -rf $HOME/.config/nvim
-check_error
 ln -s $DOTDIR/nvim $HOME/.config/nvim
-check_error
 rm -rf $HOME/.config/wezterm
-check_error
 ln -s $DOTDIR/wezterm $HOME/.config/wezterm
-check_error
-
-# zed is a special case
-# ln -sf $DOTDIR/zed/keymap.json $HOME/.config/zed/keymap.json
-# check_error
-# ln -sf $DOTDIR/zed/settings.json $HOME/.config/zed/settings.json
-# check_error
-# rm -rf $HOME/.config/zed/snippets
-# check_error
-# ln -s $DOTDIR/zed/snippets $HOME/.config/zed/snippets
-# check_error
 
 # install omz
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
