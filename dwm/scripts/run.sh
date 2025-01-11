047d428976709f912451f85de2d02d7b25c93c59
aileks <aeverly14@pm.me>
2025-01-11 16:59:26 -0500

    set resolution in run.sh

--- dwm/scripts/run.sh
+++ dwm/scripts/run.sh
@@ -4,6 +4,7 @@ xrdb merge ~/.Xresources
 feh --bg-fill ~/Pictures/Wallpapers/flowers.jpeg &
 xset r rate 200 50 &
 picom &
+xrandr --output DisplayPort-5 --mode 2560x1440 --rate 165.08 --set TearFree on
 
 dash ~/.config/dwm/scripts/bar.sh &
 while type chadwm >/dev/null; do chadwm && continue || break; done
