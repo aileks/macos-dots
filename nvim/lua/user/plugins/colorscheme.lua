20d7d57cbbecab39cf84eb6eed64045920f6a0a7
aileks <aeverly14@pm.me>
2025-01-11 15:59:08 -0500

    back on arch

--- nvim/lua/user/plugins/colorscheme.lua
+++ nvim/lua/user/plugins/colorscheme.lua
@@ -1,10 +1,12 @@
 return {
-  'Mofiqul/dracula.nvim',
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000,
   config = function ()
-    require('dracula').setup({
-      transparent_bg = true
+    require('catppuccin').setup({
+      transparent_background = true
     })
 
-    vim.cmd.colorscheme('dracula')
+    vim.cmd.colorscheme('catppuccin')
   end
 }
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,10 +5,10 @@ local config = {}
 -- actual config
 config.font = wezterm.font('BerkeleyMono Nerd Font')
 config.font_size = 16
-config.color_scheme = 'Ayu Dark (Gogh)'
+config.color_scheme = 'Catppuccin Mocha'
 config.enable_tab_bar = false
 config.window_background_opacity = 0.95
 config.window_close_confirmation = 'NeverPrompt'
--- config.window_decorations = 'RESIZE'
+config.window_decorations = 'RESIZE'
 
 return config
