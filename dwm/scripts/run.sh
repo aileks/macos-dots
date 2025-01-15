58afc1062a79166860c85037bc6d895428d11872
aileks <aeverly14@pm.me>
2025-01-14 21:17:17 -0500

    added nm-tray to run.sh

--- dwm/scripts/run.sh
+++ dwm/scripts/run.sh
@@ -6,6 +6,7 @@ xset r rate 200 60 &
 picom &
 dash ~/.config/dwm/scripts/bar.sh &
 nvidia-settings --load-config-only
+nm-tray &
 xss-lock -- i3lock-fancy -pf JetBrains-Mono-SemiBold-Italic &
 
 while type chadwm >/dev/null; do chadwm && continue || break; done
