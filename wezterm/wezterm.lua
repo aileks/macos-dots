d43477e8aac5f64c78bc1023059a21c8c5c86fbd
Liyah <aileks@jingwei.local>
2025-01-29 13:14:49 -0500

    mac branch init

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,11 +4,11 @@ local config = {}
 
 -- actual config
 config.font = wezterm.font('BerkeleyMono Nerd Font')
-config.font_size = 16
-config.color_scheme = 'Catppuccin Mocha'
+config.font_size = 18
+config.color_scheme = 'Oxocarbon Dark'
 config.enable_tab_bar = false
 config.window_background_opacity = 0.95
 config.window_close_confirmation = 'NeverPrompt'
--- config.window_decorations = 'NONE'
+config.window_decorations = 'RESIZE'
 
 return config
