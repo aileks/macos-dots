7a186b3448ab31f86e853ec595307342f9ab1fcf
Aaliyah Harper <aeverly14@pm.me>
2025-01-30 17:10:50 -0500

    linux my beloved

--- setup.sh
+++ setup.sh
@@ -8,22 +8,12 @@ DOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # symlinks
 ln -sf $DOTDIR/tmux/tmux.conf $HOME/.tmux.conf
 ln -sf $DOTDIR/zsh/zshrc $HOME/.zshrc
-ln -sf $DOTDIR/ideavim/ideavimrc $HOME/.ideavimrc
 rm -rf $HOME/.config/nvim
 ln -s $DOTDIR/nvim $HOME/.config/nvim
 rm -rf $HOME/.config/wezterm
 ln -s $DOTDIR/wezterm $HOME/.config/wezterm
-rm -rf $HOME/.config/dunst
-ln -s $DOTDIR/dunst $HOME/.config/dunst
-rm -rf $HOME/.config/dwm
-ln -s $DOTDIR/dwm $HOME/.config/dwm
 rm -rf $HOME/.config/fastfetch
 ln -s $DOTDIR/fastfetch $HOME/.config/fastfetch
-
-# install dwm
-echo "Compiling dwm, sudo password needed!"
-echo ""
-cd $DOTDIR/dwm && sudo make install
 
 # install omz
 echo "Installing Oh My Zsh"
