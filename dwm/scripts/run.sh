86843b14986f2c0622bc0c712efc735aedc10e06
Aaliyah Harper <aeverly14@pm.me>
2025-02-03 19:13:25 -0500

    ff config

--- dwm/scripts/run.sh
+++ dwm/scripts/run.sh
@@ -6,7 +6,6 @@ feh --no-fehbg --bg-fill ~/Pictures/Wallpaper/wallpaper.png &
 xset r rate 200 50 &
 picom -b &
 dash ~/.config/dwm/scripts/bar.sh &
-# nvidia-settings --load-config-only &
 nm-tray &
 xss-lock -- i3lock-fancy -pf BerkeleyMono-Nerd-Font &
 
