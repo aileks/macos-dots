652e9e308b723746ef38dc2b00c5acfd4f2a242a
Aaliyah Harper <aeverly14@pm.me>
2025-02-03 19:19:54 -0500

    fcitx5 added to run script

--- dwm/scripts/run.sh
+++ dwm/scripts/run.sh
@@ -2,11 +2,12 @@
 
 xrandr --output DP-2 --mode 2560x1440 --rate 165.08
 xrdb merge ~/.Xresources
-feh --no-fehbg --bg-fill ~/Pictures/Wallpaper/wallpaper.png &
 xset r rate 200 50 &
-picom -b &
+feh --no-fehbg --bg-fill ~/Pictures/Wallpaper/wallpaper.png &
 dash ~/.config/dwm/scripts/bar.sh &
 nm-tray &
 xss-lock -- i3lock-fancy -pf BerkeleyMono-Nerd-Font &
+picom -b
+fcitx5 -d
 
 while type chadwm >/dev/null; do chadwm && continue || break; done
