28ef66086f123a151e4259f7105e5b81ac0eb415
Aaliyah Harper <aeverly14@pm.me>
2025-02-06 07:24:24 -0500

    owo

--- dwm/scripts/run.sh
+++ dwm/scripts/run.sh
@@ -2,12 +2,12 @@
 
 xrandr --output DP-2 --mode 2560x1440 --rate 165.08
 xrdb merge ~/.Xresources
-xset r rate 200 50 &
+xset r rate 200 50
+xss-lock -- i3lock-fancy -pf BerkeleyMono-Nerd-Font &
 feh --no-fehbg --bg-fill ~/Pictures/Wallpaper/wallpaper.png &
 dash ~/.config/dwm/scripts/bar.sh &
-nm-tray &
-xss-lock -- i3lock-fancy -pf BerkeleyMono-Nerd-Font &
 picom -b
 fcitx5 -d
+nm-applet &
 
 while type chadwm >/dev/null; do chadwm && continue || break; done
